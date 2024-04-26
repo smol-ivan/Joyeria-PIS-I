5bc0b706e93d231a3f3024f432b02cb12a0d5c1c
Ivan Gordillo <ivan.gordillo@cua.uam.mx>
2024-04-25 21:46:08 -0600

    fix id initialization

--- Modelo/G_usuario/Administrador.py
+++ Modelo/G_usuario/Administrador.py
@@ -1,5 +1,5 @@
 from .Usuario import Usuario
 
 class Administrador(Usuario):
-    def __init__(self, nombre, correo, contrasena) -> None:
-        super().__init__(nombre, correo, contrasena)
+    def __init__(self, nombre, correo, contrasena, id_usuario):
+        super().__init__(nombre, correo, contrasena, id_usuario)
