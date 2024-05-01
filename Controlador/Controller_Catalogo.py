cfd6f6e1eda31783118bac5327e7e87e090d67c5
Ivan Gordillo <ivan.gordillo@cua.uam.mx>
2024-05-01 17:53:32 -0600

    simplificar logica catalogo

--- Controlador/Controller_Catalogo.py
+++ Controlador/Controller_Catalogo.py
@@ -5,7 +5,7 @@ class Controller_Catalogo:
         self.gestor_catalogo = GestorCatalogo()
 
     def boton_mostrar_catalogo(self, tipo_producto):
-        catalogo = self.gestor_catalogo.solicitar_catalogo(tipo_producto)
+        catalogo = self.gestor_catalogo.recibir_dato_catalogo(tipo_producto)
         return catalogo
     
     def boton_buscar_producto(self, dato):
--- Modelo/G_catalogo/TablaCatalogo.py
+++ Modelo/G_catalogo/TablaCatalogo.py
@@ -1,36 +1,37 @@
 from Modelo.G_inventario.Inventario import Inventario
+from Modelo.G_inventario.Producto import Aretes, Collares, Anillos, Piercings, Pulseras, Dijes
 
 class TablaCatalogo():
     def __init__(self):
-        self.productos_inventario = Inventario().obtener_lista_productos()
-        '''
-        Estructura diccionario
-        {
-            catalogo_Aretes: Instancia de Catalogo,
-            catalogo_Collares: Instancia de Catalogo,
-            catalogo_Pulseras: Instancia de Catalogo,
-            ...
-        }
-        '''
-        self.catalogos = {}
+        self.productos_inventario = None
+        self.obtener_productos_inventario()
 
     def obtener_productos_inventario(self):
-        arr = self.productos_inventario
-        return arr
-    
-    def agregar_catalogo(self, catalogo, tipo_producto):
-        self.catalogos[tipo_producto] = catalogo
+        self.productos_inventario = Inventario().obtener_lista_productos()
 
     def obtener_catalogo(self, tipo_producto):
-        return self.catalogos[tipo_producto]
+        lista_productos = []
+        for producto in self.productos_inventario:
+            if tipo_producto == "Aretes" and isinstance(producto, Aretes):
+                lista_productos.append(producto)
+            elif tipo_producto == "Collares" and isinstance(producto, Collares):
+                lista_productos.append(producto)
+            elif tipo_producto == "Anillos" and isinstance(producto, Anillos):
+                lista_productos.append(producto)
+            elif tipo_producto == "Piercings" and isinstance(producto, Piercings):
+                lista_productos.append(producto)
+            elif tipo_producto == "Pulseras" and isinstance(producto, Pulseras):
+                lista_productos.append(producto)
+            elif tipo_producto == "Dijes" and isinstance(producto, Dijes):
+                lista_productos.append(producto)
+        return lista_productos
     
     '''
     Metodo que regresa una lista de productos que coinciden con el dato de busqueda
     '''
     def buscar_producto(self, dato):
         productos = []
-        for catalogo in self.catalogos.values():
-            for producto in catalogo.obtener_productos():
-                if producto.get_nombre().lower() == dato.lower() or producto.get_marca().lower() == dato.lower() or producto.get_material().lower() == dato.lower() or producto.get_color().lower() == dato.lower() or producto.get_piedra().lower() == dato.lower():
-                    productos.append(producto)
+        for producto in self.productos_inventario:
+            if producto.get_nombre().lower() == dato.lower() or producto.get_marca().lower() == dato.lower() or producto.get_material().lower() == dato.lower() or producto.get_color().lower() == dato.lower() or producto.get_piedra().lower() == dato.lower():
+                productos.append(producto)
         return productos
