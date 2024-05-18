f1c3141e9434b82856e54fd9f7358cf4b4e8051d
Ivan Gordillo <ivan.gordillo@cua.uam.mx>
2024-05-18 12:18:22 -0600

    fix session call

--- Vista/UI_Carrito.py
+++ Vista/UI_Carrito.py
@@ -61,8 +61,9 @@ class UI_Carrito:
                 print("\nCarrito vacío")
                 break
             print("\nProductos en el carrito:")
-            for producto in carrito:
-                print(f"Producto: {producto['nombre']} Precio: {producto['precio']}")
+            # for producto in carrito.obtener_carrito():
+            #     print(f"Producto: {producto['nombre']} Precio: {producto['precio']}")
+            carrito.mostrar_carrito()
             modelo = input("\nIngrese el modelo del producto a eliminar: ")
             if modelo:
                 if self.controller.eliminar_producto(modelo):
