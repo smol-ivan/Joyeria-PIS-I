ddf64da09dd017d636f8218bfdb482d3ad86dd53
Ivan Gordillo <ivan.gordillo@cua.uam.mx>
2024-05-20 19:48:57 -0600

    Base macrofuncionalidad compra

--- Vista/UI_Compra.py
+++ Vista/UI_Compra.py
@@ -11,3 +11,14 @@ class UI_Compra:
         '''Metodo que despliega el menu de compras
         '''
         pass
+
+    """ 
+    Pedir al controlador si existen datos de envio de usuario y forma de pago
+
+    if controlador.not puede_comprar():
+        print("No tienes datos de envio o de pago. por favor agrega datos antes de realizar la compra")
+    # Si hay datos envio y pago
+    Muestras la ui de compra
+    controlador.realixar_compra()
+
+    """
