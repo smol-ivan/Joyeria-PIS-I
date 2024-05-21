63e4a97ccffc10eb422f0493a888bbf8cde24de6
Brandon Aguirre <brandon.aguirre@cua.uam.mx>
2024-05-21 01:05:21 -0600

    "UI COMPRA"

--- Vista/UI_Compra.py
+++ Vista/UI_Compra.py
@@ -1,24 +1,38 @@
-# Import controlador
+from Controlador.Controller_Compra import Controller_Compra
 
 class UI_Compra:
     '''Clase que despliega la interfaz de compras
     '''
-    def __init__(self) -> None:
-        # controlador como atributo
-        pass
+    def __init__(self):
+        self.controlador = Controller_Compra()
 
-    def menu_compra(self, productos: list[dict] = None) -> None:
-        '''Metodo que despliega el menu de compras
+    def menu_compra(self):
+        print("Sistema de Compras")
+        print("1. Verificar Datos de Envío y Pago")
+        print("2. Realizar Compra")
+        opcion = input("Seleccione una opción: ")
+        if opcion == "1":
+            self.verificar_datos()
+        elif opcion == "2":
+            self.realizar_compra()
+        else:
+            print("Opción no válida")
+
+    def verificar_datos(self):
+        '''Método que verifica si el usuario tiene datos de envío y de pago
         '''
-        pass
+        if not self.controlador.puede_comprar():
+            print("No tienes datos de envío o de pago. Por favor, agrega datos antes de realizar la compra.")
+        else:
+            print("Tienes los datos de envío y pago completos. Puedes realizar la compra.")
 
-    """ 
-    Pedir al controlador si existen datos de envio de usuario y forma de pago
-
-    if controlador.not puede_comprar():
-        print("No tienes datos de envio o de pago. por favor agrega datos antes de realizar la compra")
-    # Si hay datos envio y pago
-    Muestras la ui de compra
-    controlador.realixar_compra()
-
-    """
+    def realizar_compra(self):
+        '''Método que realiza la compra
+        '''
+        if self.controlador.puede_comprar():
+            if self.controlador.realizar_compras():
+                print("Compra realizada con éxito.")
+            else:
+                print("No se pudo realizar la compra.")
+        else:
+            print("No tienes datos de envío o de pago. Por favor, agrega datos antes de realizar la compra.")
